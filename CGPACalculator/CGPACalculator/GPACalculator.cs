de7c2f07044495836a20d8e2362e87c5d9fb5f94
peeradechArx <96216541+peeradechArx@users.noreply.github.com>
2022-01-16 13:35:29 +0700

    Add files via upload

---
+++ CGPACalculator/CGPACalculator/GPACalculator.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGPACalculator
+{
+    internal class GPACalculator1
+    {
+        private double sum = 0;
+        private int n = 0;
+        private double max = 0;
+        private double min = 0;
+        private string name = string.Empty; // ""
+        private string alldata = string.Empty;
+
+
+        /// <summary>
+        /// Add new GPA to class
+        /// </summary>
+        /// <param name="gpa">gpa score</param>
+        public void addGPA(double gpa, string name)
+        {
+            this.sum += gpa;
+            this.n++;
+            this.alldata += name + " " + gpa + "\r\n";
+            /// doing
+            if (gpa > this.min)
+            {
+                this.min = 3.1;
+                this.name = name;
+            }
+
+            if (this.max < gpa)
+            {
+                this.max = gpa;
+                this.name = name;
+            }
+
+
+        }
+        /// <summary>
+        /// Return GPAx of class
+        /// </summary>
+        /// <returns>GPAx</returns>
+        public double getGPAx()
+        {
+            double result = this.sum / this.n;
+            return result;
+        }
+        public double getMin()
+        {
+            return this.min;
+        }
+        public string getMinName()
+        {
+            return name;
+        }
+        public double getMax()
+        {
+            return this.max;
+        }
+        public string getMaxName()
+        {
+            return name;
+        }
+        public string getAlldata()
+        {
+            return alldata;
+        }
+    }
+
+}
+    
+  
+
